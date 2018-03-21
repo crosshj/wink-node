cc988e22d17a77426671fd93cc21dea089ef29f9
Harrison Cross <github@chimpjuice.com>
2018-03-21 00:03:26 -0400

    add sop, routes cleanup, notes

--- homeAuto.js
+++ homeAuto.js
@@ -9,73 +9,66 @@ app.use(flatiron.plugins.http);
 process.title = 'Home Automation Service';
 
 var routes = [{
+  path: '/',
+  headers: { 'Content-Type': 'text/html' },
+  html: '' +
+    '</br><p>--- NOT IMPLEMENTED ---</p>' +
+    '<p>group CRUD?</p>' +
+    '<p>link controller to group/lights?</p>' +
+    '<p>force refresh?</p>' +
+    '<p>test radio/device?</p>' +
+    ''
+}, {
   path: '/options',
   headers: { 'Content-Type': 'text/plain' },
   commandArgs: '',
-}, {
-  path: '/devices/all',
-  headers: { 'Content-Type': 'text/plain' },
-  commandArgs: '-l',
 }, {
   path: '/add/zigbee',
   headers: { 'Content-Type': 'text/plain' },
   commandArgs: '-a -r zigbee',
 }, {
+  path: '/devices/all',
+  headers: { 'Content-Type': 'text/plain' },
+  commandArgs: '-l',
+}, 
+// parameterized routes
+{
+  path: '/devices/:deviceId',
+  headers: { 'Content-Type': 'text/plain' },
+  commandArgs: '-l -m {arg}',
+}, {
+  // double underscore is replaced by space
   path: '/rename/:deviceId/:name',
   commandArgs: '-m {arg} --set-name "{arg}"'
+}, {
+  path: '/change/:deviceId/:attr/:value',
+  commandArgs: '-u -m {arg} -t {arg} -v {arg}'
+}, {
+  path: '/delete/:deviceId',
+  commandArgs: '-m {arg} -d'
 }];
 
-var routesHtml = routes.reduce(function(all, x){
-  return all + '<p><a href="' + x.path + '">GET: ' + x.path + '</a></p>';
-}, '')
-
-app.router.get('/', function () {
-  this.res.writeHead(200, { 'Content-Type': 'text/html' });
-  this.res.end(routesHtml +
-    '<p>GET: /change/:device/:value</p>' +
-    '<p>--- NOT IMPLEMENTED ---</p>' +
-    '<p>PUT: /devices/</p>' +
-    '<p>GET: /devices/{deviceId}</p>' +
-    '<p>UPDATE: /devices/{deviceId}</p>' +
-    '<p>DEL: /devices/{deviceId}</p>' +
-  '');
-});
+var routesHtml = routes
+  .filter(function(el, i){ return i > 0; })
+  .reduce(function(all, x){
+    return all + '<p><a href="' + x.path + '">' + x.path + '</a></p>';
+  }, '');
 
 routes.forEach(function(x) {
   app.router.get(x.path, function () {
     var that = this;
+    if(x.path === '/'){
+      this.res.end(routesHtml + (x.html || ''));
+      return;
+    }
     var commandArgs = x.commandArgs;
-    arguments.forEach(function(arg, i){
-      commandArgs = commandArgs.replace('{arg}', arguments[i]);
+    [].slice.call(arguments).forEach(function(arg, i){
+      commandArgs = commandArgs.replace('{arg}', (arg+'').replace(/__/g, ' '));
     });
     exec('aprontest ' + commandArgs, function(error, stdout, stderr) {
       that.res.writeHead(200, x.headers);
-      that.res.end(stdout + '\n\n' + commandArgs);
+      that.res.end(stdout + '\n\n Command Args:' + commandArgs);
     });
-  });
-});
-
-//aprontest -u -m 4 -t 1 -v ON
-//aprontest -u -m 4 -t 2 -v 255
-app.router.get('/change/:deviceId/:value', function (deviceId, value) {
-  var that = this;
-  if(Number(value)){
-    exec('aprontest -u -m ' + deviceId + ' -t 2 -v ' + value, function(error, stdout, stderr) {
-      that.res.writeHead(200, { 'Content-Type': 'text/plain' });
-      that.res.end(stdout);
-    });
-    return;
-  }
-  if(value === 'ON'){
-    exec('aprontest -u -m ' + deviceId + ' -t 1 -v ON', function(error, stdout, stderr) {
-      that.res.writeHead(200, { 'Content-Type': 'text/plain' });
-      that.res.end(stdout);
-    });
-    return;
-  }
-  exec('aprontest -u -m ' + deviceId + ' -t 1 -v OFF', function(error, stdout, stderr) {
-    that.res.writeHead(200, { 'Content-Type': 'text/plain' });
-    that.res.end(stdout);
   });
 });
 
